aad58596af09875726ac682d88f039d4e834e88e
albert <xazivalieva2@gmail.com>
2023-11-28 20:51:10 +0300

    version 1

--- прямоугольник.py
+++ прямоугольник.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     a = input().split()
     size = x, y = int(a[0]), int(a[1])
     screen = pygame.display.set_mode(size)
+    screen.fill(pygame.Color('red'), pygame.Rect(1, 1, x - 1, y - 1))
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
